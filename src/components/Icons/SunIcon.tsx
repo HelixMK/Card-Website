f90162aa88cca3902c3be962d15f9e3d52a32777
Maksim Kasatkin <helix.team.mk@gmail.com>
2023-12-08 14:41:25 +0300

    fix

--- src/components/Icons/SunIcon.tsx
+++ src/components/Icons/SunIcon.tsx
@@ -9,9 +9,9 @@ const SunIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="rotate-0 scale-100 transition-all duration-300  dark:-rotate-90 dark:scale-0"
     >
       <circle cx="12" cy="12" r="4"></circle>
